736dd1134008d459d9d0271a9fbc5f7f4391f2cd
Vitaly <vitvlkv@gmail.com>
2016-12-18 22:30:21 +0300

    I've found out how to use "Prefix Seek" rocksdb feature

---
+++ test/first_delim_prefix_transform.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <rocksdb/db.h>
+#include <rocksdb/slice_transform.h>
+#include <iostream>
+
+class FirstDelimPrefixTransform : public rocksdb::SliceTransform {
+    std::string _delim;
+    std::string _name;
+
+public:
+    explicit FirstDelimPrefixTransform(const std::string& delim)
+    : _delim(delim),
+    // Note that if any part of the name format changes, it will require
+    // changes on options_helper in order to make RocksDBOptionsParser work
+    // for the new change.
+    _name("reggata.FirstDelimPrefixTransform." + _delim) {
+    }
+
+    virtual const char* Name() const {
+        return _name.c_str();
+    }
+
+    virtual rocksdb::Slice Transform(const rocksdb::Slice& src) const {
+        assert(InDomain(src));
+        // TODO: optimize, do not use std::string
+        auto str = src.ToString();
+        auto i = str.find_first_of(_delim);
+        auto res = rocksdb::Slice(src.data(), i);
+        return res;
+    }
+
+    virtual bool InDomain(const rocksdb::Slice& src) const {
+        // TODO: optimize, do not use std::string
+        auto str = src.ToString();
+        return str.find_first_of(_delim) != str.size(); // TODO: Maybe we should return true for keys without a delimiter?..
+    }
+
+    virtual bool InRange(const rocksdb::Slice& dst) const {
+        return true;
+    }
+};
--- test/rocksdb_test.cpp
+++ test/rocksdb_test.cpp
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "rocksdb_merge_operator.h"
+#include "first_delim_prefix_transform.h"
 #include <reggata_exceptions.h>
 
 #include <rocksdb/db.h>
@@ -18,6 +19,7 @@ namespace fs = boost::filesystem;
 #include <memory>
 #include <iostream>
 #include <sstream>
+#include <rocksdb/slice_transform.h>
 
 class RocksDBTest : public testing::Test {
 public:
@@ -45,23 +47,48 @@ TEST_F(RocksDBTest, BasicOperations) {
     rocksdb::Options options;
     options.create_if_missing = true;
     options.merge_operator = std::shared_ptr<rocksdb::MergeOperator>(new UInt64AddOperator());
+    options.prefix_extractor = std::shared_ptr<rocksdb::SliceTransform>(new FirstDelimPrefixTransform(":"));
     rocksdb::DB* dbRaw;
     rocksdb::Status status = rocksdb::DB::Open(options, DB_PATH.string(), &dbRaw);
     std::unique_ptr<rocksdb::DB> db(dbRaw);
     ASSERT_EQ(true, status.ok()) << status.ToString();
 
+    ASSERT_EQ("tag1", options.prefix_extractor->Transform("tag1:1").ToString());
+    ASSERT_EQ("tag1", options.prefix_extractor->Transform("tag1:").ToString());
+    //ASSERT_EQ("", options.prefix_extractor->Transform("tag1").ToString());
+    
+    
     rocksdb::WriteOptions wo;
     wo.sync=true;
+    db->Put(wo, "blabla:1", "bla1");
     db->Put(wo, "tag1:1", "a");
     db->Put(wo, "tag1:2", "b");
-    db->Put(wo, "tag2:2", "c");
+    db->Put(wo, "taggg:2", "c");
+    db->Put(wo, "blabla:2", "bla2");
     
     rocksdb::ReadOptions ro;
+    ro.prefix_same_as_start = true;
     std::string val;
     rocksdb::Status s;
     s = db->Get(ro, "tag1:1", &val);
     ASSERT_EQ(true, s.ok()) << s.ToString();
     ASSERT_EQ("a", val);
+    
+    std::unique_ptr<rocksdb::Iterator> j(db->NewIterator(ro));
+    for (j->Seek("blabla:"); j->Valid(); j->Next()) {
+        std::cout << j->key().ToString() << " = " << j->value().ToString() << std::endl;
+    }
+    
+    std::unique_ptr<rocksdb::Iterator> i(db->NewIterator(ro));
+    i->Seek("tag1:");
+    ASSERT_TRUE(i->Valid());
+    ASSERT_EQ("tag1:1", i->key());
+    i->Next();
+    ASSERT_TRUE(i->Valid());
+    ASSERT_EQ("tag1:2", i->key());
+    i->Next();
+    ASSERT_FALSE(i->Valid());
+    
     
     s = db->Get(ro, "non_existent_key", &val);
     ASSERT_EQ(false, s.ok()) << s.ToString();
